2e77a4a41995feff30aa5d7becc5885ccc5403ed
TheMinja <stanleytechrepair@gmail.com>
2018-09-13 19:31:02 -0700

    Hourglass Challenge

---
+++ src/HourglassChallenge.java
@@ -0,0 +1,20 @@
+public class HourglassChallenge {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void createHourglass(int size){
+        int i = size;
+
+
+    }
+
+    public String createTab(int size){
+        String tab = "";
+        for (int i = 0; i < size; i++) {
+            tab = tab + "\t";
+        }
+        return tab;
+    }
+}
